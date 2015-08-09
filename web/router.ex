f9a30d5f4e295ec4745bc050a8cdef3e8b04692d
David Whitlock <alovedalongthe@gmail.com>
2015-08-09 11:03:02 +0700

    Added secure headers plug

--- web/router.ex
+++ web/router.ex
@@ -6,6 +6,7 @@ defmodule Welcome.Router do
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
+    plug :put_secure_browser_headers
     plug Openmaize.LoginoutCheck
     plug Openmaize.Authenticate
   end
