9c5fafcb4173a1e4690ee35c00a6ca251c6bc79e
David Whitlock <alovedalongthe@gmail.com>
2016-01-10 11:29:21 +0700

    Updated to work with version 0.12

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Welcome.Mixfile do
 
   def project do
     [app: :welcome,
-     version: "0.4.0",
-     elixir: "~> 1.0",
+     version: "0.5.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
